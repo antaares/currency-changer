ebcd76b32655125dee5db941e1642d8b45a9f3d1
antaares <jrismoilov@gmail.com>
2023-05-25 09:32:12 +0500

    edit wallet for demo

--- handlers/users/wallets.py
+++ handlers/users/wallets.py
@@ -12,8 +12,36 @@ TEXT = {
     'main': {
         'uz': "Sizni hamyonlaringiz",
         'ru': "Ваши кошельки"
-},
-}    
+        },
+    'enter_number': {
+        'uz': "{wallet_name} hamyon raqamini kiriting",
+        'ru': "Введите номер кошелька"
+        },
+    'wrong_number': {
+        'uz': "Siz noto'g'ri raqam kiritdingiz, qayta kiriting:",
+        'ru': "Вы ввели неправильный номер, введите еще раз:"
+        },
+    'finish_entry': {
+        'uz': "Sizning hamyon raqamingiz saqlandi.",
+        'ru': "Ваш номер кошелька сохранен."
+    }    
+}
+
+
+
+# on etime state
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
+
+
+class EntryWallet(StatesGroup):
+    choose_wallet = State()
+    enter_number = State()
+
+
+
+
+
 
 
 
@@ -22,4 +50,29 @@ TEXT = {
 async def wallets_page(message: types.Message):
     # lang = await db.get_lang(message.chat.id)
     lang = "uz"
+    await message.answer(TEXT['main'][lang], reply_markup= await WalletsPage(lang))
+    await EntryWallet.choose_wallet.set()
+
+
+
+
+@dp.message_handler(state=EntryWallet.choose_wallet)
+async def choose_wallet(message: types.Message, state: FSMContext):
+    lang = 'uz' #await db.get_lang(call.message.chat.id)
+    await message.answer(TEXT['enter_number'][lang].format(wallet_name=message.text), reply_markup= await WalletsPage(lang))
+    await EntryWallet.enter_number.set()
+
+
+
+
+@dp.message_handler(state=EntryWallet.enter_number)
+async def enter_number(message: types.Message, state: FSMContext):
+
+    lang = 'uz'
+
+    is_valid = True # await check_wallet(message.text)
+    if is_valid is False:
+        await message.answer(TEXT['wrong_number'][lang], reply_markup= await WalletsPage(lang))
+        return await EntryWallet.enter_number.set()
+    await message.answer(TEXT['finish_entry'][lang])
     await message.answer(TEXT['main'][lang], reply_markup= await WalletsPage(lang))
